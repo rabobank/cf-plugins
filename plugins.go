dbaad4c8fbbd5e1866ff2bd336bd5bf1a4c6ff6d
João Viegas <joao.nascimento@rabobank.nl>
2025-10-14 16:03:44 +0200

    support enhanced Execute plugin

--- plugins.go
+++ plugins.go
@@ -3,7 +3,8 @@ package plugins
 import "code.cloudfoundry.org/cli/plugin"
 
 type Plugin interface {
-	plugin.Plugin
+	Execute(CliConnection, []string)
+	GetMetadata() plugin.PluginMetadata
 }
 
 type pluginWrapper struct {
@@ -22,6 +23,26 @@ func (p *pluginWrapper) GetMetadata() plugin.PluginMetadata {
 	return p.plugin.GetMetadata()
 }
 
+type pluginConverter struct {
+	plugin Plugin
+}
+
+func (p *pluginConverter) Run(cliConnection plugin.CliConnection, args []string) {
+	connection, e := newCliConnection(cliConnection)
+	if e != nil {
+		panic(e)
+	}
+	p.plugin.Execute(connection, args)
+}
+
+func (p *pluginConverter) GetMetadata() plugin.PluginMetadata {
+	return p.plugin.GetMetadata()
+}
+
 func Start(cmd plugin.Plugin) {
 	plugin.Start(&pluginWrapper{cmd})
+}
+
+func Execute(cmd Plugin) {
+	plugin.Start(&pluginConverter{cmd})
 }
